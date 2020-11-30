7deb7738504d23a8349632bfbcf42209774a015c
Samuel Soares Santos <samuel.soares_santos@bordeaux-inp.fr>
2020-11-30 09:06:47 +0100

    Première version avec la détection du masque mal-porté

---
+++ 3class_ssd_video.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 22:22:52 2020
+
+@author: Sophie
+"""
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+from detect_mask import *
+from cv2 import VideoCapture
+detector = cv2.dnn.readNetFromCaffe("deploy.prototxt.txt" , "res10_300x300_ssd_iter_140000.caffemodel")
+
+video = cv2.VideoCapture(0)
+#video = cv2.VideoCapture("couvre-feu-a-paris-je-crois-que-je-vais-beaucoup-decoucher.mp3")
+
+a = 0
+
+model = load_model(weights_dir = 'weights/3class_MobileNetV2_V2_20_epochs_64.h5',n_classes = 3)
+
+while True:
+    a = a+1
+    check, frame2 = video.read()
+    #print(check)
+    #print(frame2)
+
+    base_img = frame2.copy()
+    original_size = base_img.shape
+    target_size = (300, 300)
+    frame = cv2.resize(frame2, target_size)
+    aspect_ratio_x = (original_size[1] / target_size[1])
+    aspect_ratio_y = (original_size[0] / target_size[0])  
+    
+    imageBlob = cv2.dnn.blobFromImage(image = frame)
+    detector.setInput(imageBlob)
+    faces = detector.forward()
+    
+    column_labels = ["img_id", "is_face", "confidence", "left", "top", "right", "bottom"]
+    detections_df = pd.DataFrame(faces[0][0], columns = column_labels)
+    
+    detections_df = detections_df[detections_df['is_face'] == 1]
+    detections_df = detections_df[detections_df['confidence']>0.90]
+
+    detections_df['left'] = (detections_df['left'] * 300).astype(int)
+    detections_df['bottom'] = (detections_df['bottom'] * 300).astype(int)
+    detections_df['right'] = (detections_df['right'] * 300).astype(int)
+    detections_df['top'] = (detections_df['top'] * 300).astype(int)
+    
+    for i, instance in detections_df.iterrows():
+        confidence_score = round(100*instance["confidence"], 2)
+        left = instance["left"]; right = instance["right"]
+        bottom = instance["bottom"]; top = instance["top"]
+        output = frame2[int(top*aspect_ratio_y):int(bottom*aspect_ratio_y), int(left*aspect_ratio_x): int(right*aspect_ratio_x)]
+        cv2.rectangle(frame2,(int(left*aspect_ratio_x),int(top*aspect_ratio_y)),(int(left*aspect_ratio_x)+int(right*aspect_ratio_x)-int(left*aspect_ratio_x),int(top*aspect_ratio_y)+int(bottom*aspect_ratio_y)-int(top*aspect_ratio_y)),(255,255,255),2)
+        #print(output)
+        if confidence_score>90:
+            #A COMPLETER
+            predictions = detect_mask_3_classes(model,output)
+
+            if predictions[0] >= 0.33:
+                print(f'no mask, iteration {a}')
+                upper_text = f'{100*predictions[0]:2.0f}% certainty of no mask presence'
+                cv2.rectangle(frame2,(int(left*aspect_ratio_x),int(top*aspect_ratio_y)),(int(left*aspect_ratio_x)+int(right*aspect_ratio_x)-int(left*aspect_ratio_x),int(top*aspect_ratio_y)+int(bottom*aspect_ratio_y)-int(top*aspect_ratio_y)),(0,0,255),2)
+                cv2.putText(frame2,upper_text,(int(left*aspect_ratio_x),int(top*aspect_ratio_y) - 10 ), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+            elif predictions[1] > 0.33:
+                print(f'mask, iteration {a}')
+                upper_text = f'{100*predictions[1]:2.0f}% certainty of mask presence'
+                cv2.rectangle(frame2,(int(left*aspect_ratio_x),int(top*aspect_ratio_y)),(int(left*aspect_ratio_x)+int(right*aspect_ratio_x)-int(left*aspect_ratio_x),int(top*aspect_ratio_y)+int(bottom*aspect_ratio_y)-int(top*aspect_ratio_y)),(0,255,0),2)
+                cv2.putText(frame2,upper_text,(int(left*aspect_ratio_x),int(top*aspect_ratio_y) -10 ), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+            elif predictions[2] > 0.33:
+                print(f'improper mask, iteration {a}')
+                upper_text = f'{100*predictions[2]:2.0f}% certainty of improper mask presence'
+                cv2.rectangle(frame2,(int(left*aspect_ratio_x),int(top*aspect_ratio_y)),(int(left*aspect_ratio_x)+int(right*aspect_ratio_x)-int(left*aspect_ratio_x),int(top*aspect_ratio_y)+int(bottom*aspect_ratio_y)-int(top*aspect_ratio_y)),(255,0,),2)
+                cv2.putText(frame2,upper_text,(int(left*aspect_ratio_x),int(top*aspect_ratio_y) - 10 ), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+
+        roi_color = frame2[int(top*aspect_ratio_y):int(top*aspect_ratio_y)+int(bottom*aspect_ratio_y)-int(top*aspect_ratio_y), int(left*aspect_ratio_x):int(left*aspect_ratio_x)+int(left*aspect_ratio_x)-int(right*aspect_ratio_x)]         
+    cv2.imshow("Capturing", frame2)
+    
+
+        
+    #cv2.imshow("Capturing", output)
+    
+
+    #cv2.imshow("Capturing", output)
+    
+    key = cv2.waitKey(1)
+    
+    if key == ord('q'):
+        break
+    
+print(a)
+video.release()
+cv2.destroyAllWindows()
--- detect_mask.py
+++ detect_mask.py
@@ -8,33 +8,32 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import Dense, MaxPooling2D, Flatten, Dropout
 
-
-def load_model(weights_dir = 'model.h5'):
-
-    mob = MobileNetV2(
-        input_shape = (128,128,3),
-        include_top = False,
-        weights = 'imagenet',
-        classes=2,
-        classifier_activation="softmax",
+mob = MobileNetV2(
+    input_shape = (128,128,3),
+    include_top = False,
+    weights = 'imagenet',
     )
-    mob.trainable = True
-    
-    
-    # In[15]:
-    
-    
+mob.trainable = False
+
+def classifier_MobileNetV2(n_classes = 2):
+
     model = Sequential()
     model.add(mob)
     model.add(GlobalAveragePooling2D())
     model.add(Dense(64,activation='relu'))
-    model.add(Dropout(0.8))
-    model.add(Dense(2,activation='softmax'))
-    model.summary()
-    model.load_weights(weights_dir)
+    model.add(Dropout(0.3))
+    model.add(Dense(n_classes,activation='softmax'))
+
+    return model
 
 
-def detect_mask(model,img):
+def load_model(weights_dir = 'weights/test.h5',n_classes = 2):
+    model = classifier_MobileNetV2(n_classes);
+    model.load_weights(weights_dir);
+    return model
+    
+
+def detect_mask(model,img ):
 
 	try: 
 
@@ -46,3 +45,17 @@ def detect_mask(model,img):
 	except Exception as e:
 		print('Mask detection: exception ! ')
 		return [-1, -1]
+
+
+def detect_mask_3_classes(model,img):
+
+    try: 
+
+        img_resized = cv2.resize(img, (128, 128) ,interpolation = cv2.INTER_CUBIC)
+        img_resized = (img_resized[np.newaxis,:]/255.).astype('float32')
+        prediction =  model.predict(img_resized)
+        return [prediction[0,0] , prediction[0,1] ,prediction[0,2] ]
+
+    except Exception as e:
+        print('Mask detection: exception ! ')
+        return [-1, -1, -1]
